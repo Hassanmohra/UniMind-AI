c83147648c94a9b6fe0eaa8d0ec84e91ffcdf5c5
Hassanmohra <143335526+Hassanmohra@users.noreply.github.com>
2026-09-18 22:44:58 +0300

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,15 @@
+const startBtn = document.getElementById("startBtn");
+const demoBtn = document.getElementById("demoBtn");
+const loginBtn = document.getElementById("loginBtn");
+
+startBtn.addEventListener("click", () => {
+    alert("Welcome to UniMind AI 🎓");
+});
+
+demoBtn.addEventListener("click", () => {
+    alert("Demo mode will be available soon.");
+});
+
+loginBtn.addEventListener("click", () => {
+    alert("Student login will be available soon.");
+});
